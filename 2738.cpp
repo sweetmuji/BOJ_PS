a1186fc58d47f2ccbeddc4e6d064f012941f35f0
sweetmuji <wonj1085@naver.com>
2025-09-12 10:44:10 +0900

    Add solution for matrix addition problem
    Implements a program that reads two matrices, adds them element-wise, and outputs the result. Includes input handling and efficient I/O setup.

---
+++ 2738.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+
+    cin >> N >> M;
+
+    int matrix[100][100] = { 0, };
+    int matrix2[100][100] = { 0, };
+
+    for(int i = 0; i < M; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    for(int i = 0; i < M; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            cin >> matrix2[i][j];
+        }
+    }
+
+    for(int i = 0; i < M; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            matrix2[i][j] += matrix[i][j];
+        }
+    }
+
+    for(int i = 0; i < M; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            cout << matrix2[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
